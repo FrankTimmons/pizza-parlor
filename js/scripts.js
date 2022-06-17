86a4ca38d0935c4d7401172116c2978edec766f5
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-17 11:55:37 -0700

    Add some styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Business Logic
+
 function Pizza(toppings, size){
   this.toppings = toppings;
   this.size = size;
@@ -23,16 +25,16 @@ Pizza.prototype.priceCalc = function(){
   this.price += toppingPrice;
 }
 
+//UI Logic
+
 $(document).ready(function(){
   $("form#pizzaBuilder").submit(function(event) {
     event.preventDefault();
     let toppingArray = [];
     let size = $("input:radio[name=size]:checked").val()
-
     $("input:checkbox[name=topping]:checked").each(function(){
       toppingArray.push($(this).val());
     });
-
     let pizza = new Pizza(toppingArray, size);
     pizza.priceCalc();
     $(".pizza").show();
