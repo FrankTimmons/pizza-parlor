2738848d55f860f7350ac93f2a568110c16ad36e
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-17 14:12:19 -0700

    Add a slice preview when you make your pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -27,15 +27,50 @@ Pizza.prototype.priceCalc = function(){
 
 //UI Logic
 
-$(document).ready(function(){
+function checkPizza(array){
+  let toppingArray = array;
+  console.log(toppingArray);
+  if (toppingArray.includes("Pepperoni") && toppingArray.includes("Sausage") && toppingArray.includes("Bell Peppers")){
+    $("#everything").show();
+  }else if (toppingArray.includes("Pepperoni") && toppingArray.includes("Sausage")){
+    $("#meatlover").show();
+  }else if (toppingArray.includes("Pepperoni") && toppingArray.includes("Bell Peppers")){
+    $("#pep-n-bell").show();
+  }else if (toppingArray.includes("Sausage") && toppingArray.includes("Bell Peppers")){
+    $("#sausage-n-bell").show();
+  }else if (toppingArray.includes("Pepperoni")){
+    $("#pep").show();
+  }else if (toppingArray.includes("Sausage")){
+    $("#sausage").show();
+  }else if (toppingArray.includes("Bell Peppers")){
+    $("#bell").show();
+  }else {
+    $("#cheese").show();
+  }
+}
+
+function turnOffPizza(){
+  $("#everything").hide();
+  $("#meatlover").hide();
+  $("#pep-n-bell").hide();
+  $("#sausage-n-bell").hide();
+  $("#pep").hide();
+  $("#sausage").hide();
+  $("#bell").hide();
+  $("#cheese").hide();
+}
+
+$(document).ready(function(){  
   $("form#pizzaBuilder").submit(function(event) {
     event.preventDefault();
+    turnOffPizza()
     let toppingArray = [];
     let size = $("input:radio[name=size]:checked").val()
     $("input:checkbox[name=topping]:checked").each(function(){
       toppingArray.push($(this).val());
     });
     let pizza = new Pizza(toppingArray, size);
+    checkPizza(toppingArray);
     pizza.priceCalc();
     $(".pizza").show();
     $(".pizza-price").text(pizza.price);
